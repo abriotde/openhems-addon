7a8bde7554edad7fdf88df46d9a0a2d63cedb42f
alberic <alberic.briot@osiris-team.fr>
2025-02-21 18:20:36 +0100

    1° Fail run ./test.sh : Could not open file /tmp/build_config/config.json 2° : Fail run "docker build -t openhems-addon:main ." $ pip install scikit_learn (for openhems) : "meson-python: error: Could not find ninja version 1.8.2 or newer" $ pip install mesonpy : ERROR: Could not find a version that satisfies the requirement mesonpy (from versions: none)

--- openhems/test.sh
+++ openhems/test.sh
@@ -7,7 +7,7 @@ docker run \
   -v ~/.docker:/root/.docker \
   ghcr.io/home-assistant/amd64-builder \
   --all \
-  -t openhems \
-  -r https://github.com/abriotde/openhems-addon \
-  -b main
+  -t /home/alberic/Documents/OpenHomeSystem/openhems-addon/openhems
+#  -r https://github.com/abriotde/openhems-addon \
+#  -b main
 
