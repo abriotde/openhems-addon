94c2d3a02eaafb7b22b3871cdfda682d8e415861
alberic <alberic.briot@osiris-team.fr>
2025-02-24 22:04:38 +0100

    Update publish_docker.yaml, try to fix it

--- openhems/test.sh
+++ openhems/test.sh
@@ -6,10 +6,10 @@ docker run \
   --rm \
   --privileged \
   -v ~/.docker:/root/.docker \
-  -v /home/alberic/Documents/OpenHomeSystem/openhems-addon/openhems:/data \
   ghcr.io/home-assistant/amd64-builder \
   --all \
-  -t /data
-#  -r https://github.com/abriotde/openhems-addon \
-#  -b main
+  -t openhems \
+  -r https://github.com/abriotde/openhems-addon \
+  -b main
 
+#  -v /home/alberic/Documents/OpenHomeSystem/openhems-addon/openhems:/data \
