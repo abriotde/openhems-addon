043a386e5aa9add50acaa7b1a95865f9f9bfe48b
alberic <alberic.briot@osiris-team.fr>
2025-02-25 22:01:14 +0100

    FIX for test.sh - docker run OK - docker buildx...

--- openhems/test.sh
+++ openhems/test.sh
@@ -2,14 +2,28 @@
 
 # docker build -t openhems-addon:main .
 
+# Initialize Buildx (if not already done)
+docker buildx create --use
+
+# Build and push all architectures
+docker buildx build \
+  --platform linux/amd64 \
+  -t ghcr.io/abriotde/openhems-addon:latest \
+  --push .
+
+exit
+
 docker run \
   --rm \
   --privileged \
   -v ~/.docker:/root/.docker \
+  -v /home/alberic/Documents/OpenHomeSystem/openhems-addon/openhems:/data \
   ghcr.io/home-assistant/amd64-builder \
   --all \
-  -t openhems \
-  -r https://github.com/abriotde/openhems-addon \
-  -b main
+  -t /data \
+  --push
+  # -t openhems \
+  # -r https://github.com/abriotde/openhems-addon \
+  # -b main
 
 #  -v /home/alberic/Documents/OpenHomeSystem/openhems-addon/openhems:/data \
