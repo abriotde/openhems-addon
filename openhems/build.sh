273d61e3d43101f02ee1390795cc95b19557a7c7
alberic <alberic.briot@osiris-team.fr>
2025-02-26 16:15:47 +0100

    Update publish_docker.yaml and build.sh

--- openhems/build.sh
+++ openhems/build.sh
@@ -19,9 +19,8 @@ else if [ "$ARG" == "run" ]; then
     --rm \
     --privileged \
     -v ~/.docker:/root/.docker \
-    ghcr.io/home-assistant/amd64-base \
+    ghcr.io/home-assistant/amd64-builder \
     --all \
-    -t /data \
     -t openhems \
     -r https://github.com/abriotde/openhems-addon \
     -b main
