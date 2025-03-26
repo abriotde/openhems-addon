bf009983a1781f10e66e526d5cfd7ccc48684a3b
alberic <alberic.briot@osiris-team.fr>
2025-03-26 21:37:41 +0100

    Try FIX pb :when add repository on Home-AssistantOS, it add, but no addon availables.

--- openhems/build.sh
+++ openhems/build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 ARG=$1
+VERSION=0.2.0
 
 if [ "$ARG" == "build" ]; then
-  docker build -t openhems-addon:main .
+  docker build -t openhems-addon:$VERSION .
+else if [ "$ARG" == "push" ]; then
+  docker login ghcr.io
+  docker tag ghcr.io/abriotde/openhems-addon:latest ghcr.io/abriotde/openhems-addon:0.2.0
+  docker push ghcr.io/abriotde/openhems-addon:$VERSION
 else if [ "$ARG" == "buildx" ]; then
   # Initialize Buildx (if not already done)
 	docker buildx create --use
@@ -11,22 +16,22 @@ else if [ "$ARG" == "buildx" ]; then
   # Build and push all architectures
   docker buildx build \
     --platform linux/amd64 \
-    -t ghcr.io/abriotde/openhems-addon:0.1.14 \
+    -t ghcr.io/abriotde/openhems-addon:$VERSION \
     --push .
   exit
 else if [ "$ARG" == "run" ]; then
   # https://developers.home-assistant.io/docs/add-ons/publishing/
   docker run \
-    --rm \
-    --privileged \
-    -v ~/.docker:/root/.docker \
-    ghcr.io/home-assistant/amd64-builder \
-    -t /home/alberic/Documents/OpenHomeSystem/openhems-addon/openhems:/data \
-    --all \
-    -t /data
+        --rm \
+        --privileged \
+        -v ~/.docker:/root/.docker -v /var/run/docker.sock:/var/run/docker.sock:ro \
+        -v /home/alberic/Documents/OpenHomeSystem/openhems-addon/openhems:/data \
+        ghcr.io/home-assistant/amd64-builder \
+        --amd64 \
+        -t /data
     # -t openhems \
     # -r https://github.com/abriotde/openhems-addon \
     # -b main
 else
   echo "Missing or wrong argument ($ARG) expecting 'build', 'buildx', or 'run'"
-fi; fi; fi
+fi; fi; fi; fi
